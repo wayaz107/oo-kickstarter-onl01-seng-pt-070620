7d4972e485b1e9c94e980c90564e3c93906253de
Warda Ayaz <wayaz107@gmail.com>
2020-08-04 04:20:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
   @backed_projects = []
 attr_reader :name, :backed_projects
+
 def initialize(name)
 @name = name
 @backed_projects = []
 end
 
 
-def back_project(title)
-project = Project.new(title)
+def back_project(project)
 @backed_projects << project
 @backed_projects << self
 end
